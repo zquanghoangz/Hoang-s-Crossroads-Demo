f45ef911fd8618a44336395e1cdc343c3dd37486
zquanghoangz <zquanghoangz@gmail.com>
2016-11-24 16:57:34 +0800

    Refactor TranfficLight
    Refactor TranfficLight to another file

--- CrossroadsDemo/CrossroadsDemo/FormView.cs
+++ CrossroadsDemo/CrossroadsDemo/FormView.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CrossroadsDemo
@@ -13,39 +8,81 @@ namespace CrossroadsDemo
     public partial class CrossroadsForm : Form
     {
         /* This code fixed size panel is 600 pixel square
+         * RedLightTime = GreenLightTime + YellowLightTime
          */
         private const int AreaSize = 600;
-        private const int RedLightTime = 10;
-        private const int GreenLightTime = 8;
-
-        public Point Start { get; set; }
-        public int[] _tranficLights;
+        private const int RedLightTime = 5;
+        private const int GreenLightTime = 3;
         private int _count;
+        //public int[] _trafficLights;
+        public List<TranfficLight> TranfficLights;
 
         public CrossroadsForm()
         {
             InitializeComponent();
 
             Start = new Point(30, 30);
-            _tranficLights = new int[4];
-            _tranficLights[0] = _tranficLights[2] = 0; //Red
-            _tranficLights[1] = _tranficLights[3] = 2; //Green
 
-            //Timer to change tranfic light
-            Timer tranficLightTimer = new Timer { Interval = 1000 }; //1s
-            tranficLightTimer.Tick += TranficLightTimerTick;
+            InitializeTrafficLight();
+
+            //Timer to change traffic light
+            var trafficLightTimer = new Timer { Interval = 1000 }; //1s
+            trafficLightTimer.Tick += TrafficLightTimerTick;
             _count = 0;
-            tranficLightTimer.Start();
+            trafficLightTimer.Start();
         }
 
-        private void TranficLightTimerTick(object sender, EventArgs e)
+        public Point Start { get; set; }
+
+        private void InitializeTrafficLight()
+        {
+            TranfficLights = new List<TranfficLight>
+            {
+                new TranfficLight
+                {
+                    LightNumber = 1,
+                    Start = new Point(Start.X + AreaSize/3 + AreaSize/48, Start.Y + AreaSize/3 - AreaSize/24),
+                    LightSize = AreaSize/24,
+                    IsHozontal = true
+                },
+                new TranfficLight
+                {
+                    LightNumber = 2,
+                    Start = new Point(Start.X + AreaSize/3 - AreaSize/24, Start.Y + AreaSize/2 + AreaSize/48),
+                    LightSize = AreaSize/24,
+                    IsHozontal = false
+                },
+                new TranfficLight
+                {
+                    LightNumber = 3,
+                    Start = new Point(Start.X + AreaSize/2 + AreaSize/48, Start.Y + 2*AreaSize/3),
+                    LightSize = AreaSize/24,
+                    IsHozontal = true
+                },
+                new TranfficLight
+                {
+                    LightNumber = 4,
+                    Start = new Point(Start.X + 2*AreaSize/3, Start.Y + AreaSize/3 + AreaSize/48),
+                    LightSize = AreaSize/24,
+                    IsHozontal = false
+                }
+            };
+            TranfficLights[0].TurnRedOn();
+            TranfficLights[1].TurnGreenOn();
+            TranfficLights[2].TurnRedOn();
+            TranfficLights[3].TurnGreenOn();
+        }
+
+        private void TrafficLightTimerTick(object sender, EventArgs e)
         {
             _count++;
             if (_count == RedLightTime)
             {
                 //Red to green, Green to red
-                _tranficLights[0] = _tranficLights[2] = _tranficLights[0] == 0 ? 2 : 0;
-                _tranficLights[1] = _tranficLights[3] = _tranficLights[1] == 0 ? 2 : 0;
+                foreach (var light in TranfficLights)
+                {
+                    light.SwitchLight();
+                }
                 //Reset count
                 _count = 0;
 
@@ -55,13 +92,9 @@ namespace CrossroadsDemo
             else if (_count == GreenLightTime)
             {
                 //Green to yellow
-                if (_tranficLights[0] == 2)
+                foreach (var light in TranfficLights)
                 {
-                    _tranficLights[0] = _tranficLights[2] = 1;
-                }
-                if (_tranficLights[1] == 2)
-                {
-                    _tranficLights[1] = _tranficLights[3] = 1;
+                    light.TurnYellowOn();
                 }
 
                 Invalidate();
@@ -70,7 +103,7 @@ namespace CrossroadsDemo
 
         private void CrossroadsForm_Paint(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics;
+            var g = e.Graphics;
 
             //Draw Area
             g.DrawRectangle(new Pen(Brushes.Black, 5), Start.X, Start.Y, AreaSize, AreaSize);
@@ -84,58 +117,17 @@ namespace CrossroadsDemo
             g.FillRectangle(Brushes.DarkGreen, 2 * fieldSize + Start.X, 2 * fieldSize + Start.Y, fieldSize, fieldSize);
 
             //Center lines
-            g.DrawLine(new Pen(Brushes.Black, 3), Start.X, AreaSize / 2 + Start.Y, AreaSize + Start.X, AreaSize / 2 + Start.Y);
-            g.DrawLine(new Pen(Brushes.Black, 3), AreaSize / 2 + Start.X, Start.Y, AreaSize / 2 + Start.X, AreaSize + Start.Y);
+            g.DrawLine(new Pen(Brushes.Black, 3), Start.X, AreaSize / 2 + Start.Y, AreaSize + Start.X,
+                AreaSize / 2 + Start.Y);
+            g.DrawLine(new Pen(Brushes.Black, 3), AreaSize / 2 + Start.X, Start.Y, AreaSize / 2 + Start.X,
+                AreaSize + Start.Y);
 
-            //Draw tranffic light
-            // * This is right side tranfic
-            DrawTranficLight(
-                g: g,
-                startX: Start.X + AreaSize / 3 + AreaSize / 48,
-                startY: Start.Y + AreaSize / 3 - AreaSize / 24,
-                size: AreaSize / 8,
-                isHozontal: true,
-                onNumber: _tranficLights[0]);
-
-            DrawTranficLight(
-                g: g,
-                startX: Start.X + AreaSize / 2 + AreaSize / 48,
-                startY: Start.Y + 2 * AreaSize / 3,
-                size: AreaSize / 8,
-                isHozontal: true,
-                onNumber: _tranficLights[2]);
-
-            DrawTranficLight(
-               g: g,
-               startX: Start.X + AreaSize / 3 - AreaSize / 24,
-               startY: Start.Y + AreaSize / 2 + AreaSize / 48,
-               size: AreaSize / 8,
-               isHozontal: false,
-               onNumber: _tranficLights[1]);
-
-            DrawTranficLight(
-               g: g,
-               startX: Start.X + 2*AreaSize / 3,
-               startY: Start.Y + AreaSize / 3 + AreaSize / 48,
-               size: AreaSize / 8,
-               isHozontal: false,
-               onNumber: _tranficLights[3]);
-        }
-
-        private void DrawTranficLight(Graphics g, int startX, int startY, int size, bool isHozontal, int onNumber)
-        {
-            int incWidth = 0, incHeight = size / 3, sizeOfLight = size / 3;
-            if (isHozontal)
+            //Draw trafffic light
+            // * This is right side traffic
+            foreach (var light in TranfficLights)
             {
-                incWidth = size / 3;
-                incHeight = 0;
+                light.Draw(g);
             }
-
-            g.FillEllipse(Brushes.Red, startX, startY, sizeOfLight, sizeOfLight);
-            g.FillEllipse(Brushes.Yellow, startX + incWidth, startY + incHeight, sizeOfLight, sizeOfLight);
-            g.FillEllipse(Brushes.Green, startX + 2 * incWidth, startY + 2 * incHeight, sizeOfLight, sizeOfLight);
-
-            g.DrawEllipse(new Pen(Brushes.Orange, 3), startX + onNumber * incWidth, startY + onNumber * incHeight, sizeOfLight, sizeOfLight);
         }
     }
 }
---
+++ CrossroadsDemo/CrossroadsDemo/TranfficLight.cs
@@ -0,0 +1,79 @@
+﻿using System.Drawing;
+
+namespace CrossroadsDemo
+{
+    public class TranfficLight
+    {
+        private int _lightOnNumber; //zero base: Red, Yellow, Green
+
+        public TranfficLight()
+        {
+            _lightOnNumber = 0;
+        }
+
+        public int LightNumber { get; set; }
+        public Point Start { get; set; }
+        public int LightSize { get; set; }
+        public bool IsHozontal { get; set; }
+
+        public void Draw(Graphics g)
+        {
+            int incWidth = 0, incHeight = LightSize;
+            if (IsHozontal)
+            {
+                incWidth = LightSize;
+                incHeight = 0;
+            }
+
+            g.FillEllipse(Brushes.Red, Start.X, Start.Y, LightSize, LightSize);
+            g.FillEllipse(Brushes.Yellow, Start.X + incWidth, Start.Y + incHeight, LightSize, LightSize);
+            g.FillEllipse(Brushes.Green, Start.X + 2 * incWidth, Start.Y + 2 * incHeight, LightSize, LightSize);
+
+            g.DrawEllipse(new Pen(Brushes.Orange, 3), Start.X + _lightOnNumber * incWidth,
+                Start.Y + _lightOnNumber * incHeight, LightSize, LightSize);
+        }
+
+        public void TurnRedOn()
+        {
+            //Yellow turn red
+            if (_lightOnNumber == 1)
+            {
+                _lightOnNumber = 0;
+            }
+        }
+
+        public void TurnYellowOn()
+        {
+            //Green turn yellow
+            if (_lightOnNumber == 2)
+            {
+                _lightOnNumber = 1;
+            }
+        }
+
+        public void TurnGreenOn()
+        {
+            //Red turn green
+            if (_lightOnNumber == 0)
+            {
+                _lightOnNumber = 2;
+            }
+        }
+
+        public void SwitchLight()
+        {
+            if (_lightOnNumber == 0)
+            {
+                _lightOnNumber = 2;
+            }
+            else if (_lightOnNumber == 1)
+            {
+                _lightOnNumber = 0;
+            }
+            else if (_lightOnNumber == 2)
+            {
+                _lightOnNumber = 1;
+            }
+        }
+    }
+}
